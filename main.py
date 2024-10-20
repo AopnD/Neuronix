605af515d89e0b7779e4eea1ebf025ffb8cd97a9
Omer Malka <aopnd@outlook.com>
2024-10-20 21:55:58 +0300

    first representation of a singel node

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Neuronix!")
+# representation of a singel node with 3 inputs ( data we getr from other nodes\ training data)
+# weights and biases pre determind for now.
+
+# inputs = [1.2, 2.3, 3.4]
+# weights = [0.7, -0.5, 1.25]
+# bias = 2
+#
+# output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+#
+# print(output)
