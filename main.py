86f95b2bce257f445cd0fe9b603836556b4fca14
Omer Malka <aopnd@outlook.com>
2024-11-16 11:36:11 +0200

    representing Sahpes and dimensions with lists

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ inputs = [1.2, 2.3, 3.4, 4.5]
 weights = [
     [0.7, -0.5, 1.25, 0.3],
     [0.8, -0.6, 1.3, -0.2],
-    [0.6, -0.4, 1.2, 0.5],
+    [0.6, -0.4, 0.62, 0.5],
     [-0.14, 0.2, -0.9, 0.7]
 ]
 ## theoraticly you can also use only biases or only weights, however this way make things a whole lot easier to calculate and change.
@@ -38,3 +38,40 @@ def get_output():
 
 
 get_output()
+
+
+## Shapes
+## At each dimension, what's the size of that dimension
+
+## One-dimensional list:
+## Shape: (1, 4) Type: 1D Array, Vector.
+# l = [7, 4, 2, 6]
+
+## Two-dimensional list:
+## Shape: (2, 4) Type: 2D Array, Matrix.
+# lol = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8]
+# ]
+
+## Three-dimensional list:
+## Shape: (3, 3, 5) Type: 3D Array.
+# lolol = [
+#     [
+#         [1, 2, 3, 4, 5],
+#         [6, 7, 8, 9, 10],
+#         [1.1, 1.2, 1.3, 1.4, 1.5]
+#     ],
+#     [
+#         [5, 4, 3, 2, 1],
+#         [10, 9, 8, 7, 6],
+#         [2.1, 2.2, 2.3, 2.4, 2.5]
+#     ],
+#     [
+#         [8, 2, 5, 1, 9],
+#         [1.7, 6, -12, 4, 10],
+#         [3.1, 3.2, 3.3, 3.4, 3.5]
+#     ]
+# ]
+
+
