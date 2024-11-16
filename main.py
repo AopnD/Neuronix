d52080fab0da7c7a9ff5a40753c97c3384139bd8
Omer Malka <aopnd@outlook.com>
2024-11-16 12:01:00 +0200

    adding the numpy .dot method and explaining what it does

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Representation of a single node with 3 inputs ( data we getr from other nodes\ training data)
 # weights and biases pre-determined for now.
 
@@ -13,31 +15,31 @@
 # and Biases
 
 ## here we have 4 neurons, resiving 3 inputs each, and returning a list of 4 new predictions from the output layer.
-inputs = [1.2, 2.3, 3.4, 4.5]
+# inputs = [1.2, 2.3, 3.4, 4.5]
 ## the number of weights needs to match the number of biases otherwise you have incomplete list.
-weights = [
-    [0.7, -0.5, 1.25, 0.3],
-    [0.8, -0.6, 1.3, -0.2],
-    [0.6, -0.4, 0.62, 0.5],
-    [-0.14, 0.2, -0.9, 0.7]
-]
+# weights = [
+#     [0.7, -0.5, 0.25, 0.3],
+#     [0.8, -0.6, 1.3, -0.2],
+#     [0.6, -0.4, 0.62, 0.5],
+#     [-0.14, 0.2, -0.9, 0.7]
+# ]
 ## theoraticly you can also use only biases or only weights, however this way make things a whole lot easier to calculate and change.
-biases = [2, 1.3, 1.25, 1]
+# biases = [2, 1.3, 1.25, 1]
 
-def get_output():
-    if len(weights) != len(biases):
-        print("Mismatch between weights and biases.")
-        return
-    results = []
-    for i in range(len(weights)):
-        weighted_sum = sum(inputs[j] * weights[i][j] for j in range(len(weights[i])))
-        result = weighted_sum + biases[i]
-        results.append(result)
-
-    print(results)
-
-
-get_output()
+# def get_output():
+#     if len(weights) != len(biases):
+#         print("Mismatch between weights and biases.")
+#         return
+#     results = []
+#     for i in range(len(weights)):
+#         weighted_sum = sum(inputs[j] * weights[i][j] for j in range(len(weights[i])))
+#         result = weighted_sum + biases[i]
+#         results.append(result)
+#
+#     print(results)
+#
+#
+# get_output()
 
 
 ## Shapes
@@ -74,4 +76,31 @@ get_output()
 #     ]
 # ]
 
+## What dot_product does:
+# Example with two 1D vectors:
+# a= [1,2,3]
+# b = [2,3,4]
+# dot_product = a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+# print(dot_product)
 
+## np.dot with two 1D vectors:
+# Explanation: This example calculates the dot product of two 1D arrays.
+# inputs = [1.2, 2.3, 3.4, 4.5]
+# weights = [0.7, -0.5, 0.25, 0.3]
+# bias = 2
+# output = np.dot(weights, inputs) + bias
+# print(output)
+
+
+## np.dot with weights as a matrix:
+# Explanation: This example shows matrix-vector multiplication using np.dot.
+inputs = [1.2, 2.3, 3.4, 4.5]
+weights = [
+    [0.7, -0.5, 0.25, 0.3],
+    [0.8, -0.6, 1.3, -0.2],
+    [0.6, -0.4, 0.62, 0.5],
+    [-0.14, 0.2, -0.9, 0.7]
+]
+biases = [2, 1.3, 1.25, 1]
+output = np.dot(weights, inputs) + biases
+print(output)
